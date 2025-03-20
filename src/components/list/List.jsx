84dcfaee7a5ee55b487f9d25d16c986629981a52
ShadowLightnin <player1.will@gmail.com>
2025-03-20 10:47:58 -0600

    newnew

--- src/components/list/List.jsx
+++ src/components/list/List.jsx
@@ -1,8 +1,13 @@
 import "./list.css"
+import Chatlist from "./chatList/Chatlist"
+import Userinfo from "./userInfo/Userinfo"
 
 const List = () => {
     return (
-        <div className='list'>List</div>
+        <div className='list'>
+            <Userinfo/>
+            <Chatlist/>
+        </div>
     )
 }
 
