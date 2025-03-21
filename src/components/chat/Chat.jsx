16d99fb88b46e85c7247972bd6ef0798f77385d4
Night Hawk <player1.will@gmail.com>
2025-03-20 22:12:49 -0600

    logintime

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -1,6 +1,22 @@
+import { useEffect, useRef, useState } from "react";
 import "./chat.css"
+import EmojiPicker from "emoji-picker-react"
 
 const Chat = () => {
+    const [open, setOpen] = useState(false);
+    const [text, setText] = useState("");
+
+    const endRef = useRef(null);
+
+    useEffect(() => {
+        endRef.current?.scrollIntoView({ behavior: "smooth" });
+    },[])
+
+    const handleEmoji = e =>{
+        setText((prev) => prev + e.emoji);
+        setOpen(false)
+    };
+
     return (
         <div className='chat'>
             <div className="top">
@@ -17,16 +33,106 @@ const Chat = () => {
                     <img src="./info.png" alt="" />
                 </div>
             </div>
-            <div className="center"></div>
+            <div className="center">
+                <div className="message">
+                    <img src="./avatar.png" alt="" />
+                    <div className="text">
+                        <p>
+                            Lore fkadhsfkahl djsfhahv jfh akhflks hfdh fka h
+                        </p>
+                        <span>1 min ago</span>
+                    </div>
+                </div>
+
+                <div className="message own">
+                    <div className="text">
+                        <p>
+                            Reply example message here...
+                        </p>
+                        <span>1 min ago</span>
+                    </div>
+                </div>
+
+                <div className="message">
+                    <img src="./avatar.png" alt="" />
+                    <div className="text">
+                        <p>
+                            Lore fkadhsfkahl djsfhahv jfh akhflks hfdh fka h
+                        </p>
+                        <span>1 min ago</span>
+                    </div>
+                </div>
+
+                <div className="message own">
+                    <div className="text">
+                        <p>
+                            Reply example message here...
+                        </p>
+                        <span>1 min ago</span>
+                    </div>
+                </div>
+
+                <div className="message">
+                    <img src="./avatar.png" alt="" />
+                    <div className="text">
+                        <p>
+                            Lore fkadhsfkahl djsfhahv jfh akhflks hfdh fka h
+                        </p>
+                        <span>1 min ago</span>
+                    </div>
+                </div>
+
+                <div className="message own">
+                    <div className="text">
+                        <p>
+                            Reply example message here...
+                        </p>
+                        <span>1 min ago</span>
+                    </div>
+                </div>
+
+                <div className="message">
+                    <img src="./avatar.png" alt="" />
+                    <div className="text">
+                        <p>
+                            Lore fkadhsfkahl djsfhahv jfh akhflks hfdh fka h
+                        </p>
+                        <span>1 min ago</span>
+                    </div>
+                </div>
+
+                <div className="message own">
+                    <div className="text">
+                        <img src="https://th.bing.com/th/id/OIP.CPCNF0WiJPREiMck_babDAHaEJ?w=292&h=180&c=7&r=0&o=5&pid=1.7" alt="" />
+                        <p>
+                            Reply example message here...
+                        </p>
+                        <span>1 min ago</span>
+                    </div>
+                </div>
+                <div ref={endRef}></div>
+            </div>
             <div className="bottom">
                 <div className="icons">
                     <img src="./img.png" alt="" />
                     <img src="./camera.png" alt="" />
                     <img src="./mic.png" alt="" />
                 </div>
-                <input type="text" placeholder="Type a message..." />
+                <input 
+                type="text" 
+                placeholder="Type a message..."
+                value={text} 
+                onChange={(e) => setText(e.target.value)} 
+                />
                 <div className="emoji">
-                    <img src="./emoji.png" alt="" />
+                    <img 
+                    src="./emoji.png" 
+                    alt="" 
+                    onClick={() => setOpen((prev) => !prev)}
+                    />
+                    <div className="picker">
+                    <EmojiPicker open={open} onEmojiClick={handleEmoji}/>
+                    </div>
                 </div>
                 <button className="sendButton">Send</button>
             </div>
--- src/components/detail/Detail.jsx
+++ src/components/detail/Detail.jsx
@@ -2,7 +2,78 @@ import "./detail.css"
 
 const Detail = () => {
     return (
-        <div className='detail'>Detail</div>
+        <div className='detail'>
+            <div className="user">
+                <img src="./avatar.png" alt="" />
+                <h2>John Doe</h2>
+                <p>Lorem ipsum dolor, sit amet.</p>
+            </div>
+            <div className="info">
+            <div className="option">
+                    <div className="title">
+                        <span>Chat Settings</span>
+                        <img src="./arrowUp.png" alt="" />
+                    </div>
+                </div>
+                <div className="option">
+                    <div className="title">
+                        <span>Privacy & help</span>
+                        <img src="./arrowUp.png" alt="" />
+                    </div>
+                </div>
+                <div className="option">
+                    <div className="title">
+                        <span>Shared PHotos</span>
+                        <img src="./arrowDown.png" alt="" />
+                    </div>
+                    <div className="photos">
+                            <div className="photoItem">
+                                <div className="photoDetail">
+                                <img src="https://th.bing.com/th/id/OIP.CPCNF0WiJPREiMck_babDAHaEJ?w=292&h=180&c=7&r=0&o=5&pid=1.7" alt="" />
+                                <span>photo_2024_2.png</span>
+                            </div>
+                            <img src="./download.png" alt="" className="icon"/>
+                        </div>
+                        <div className="photoItem">
+                                <div className="photoDetail">
+                                <img src="https://th.bing.com/th/id/OIP.CPCNF0WiJPREiMck_babDAHaEJ?w=292&h=180&c=7&r=0&o=5&pid=1.7" alt="" />
+                                <span>photo_2024_2.png</span>
+                            </div>
+                            <img src="./download.png" alt="" className="icon"/>
+                        </div>
+                        <div className="photoItem">
+                                <div className="photoDetail">
+                                <img src="https://th.bing.com/th/id/OIP.CPCNF0WiJPREiMck_babDAHaEJ?w=292&h=180&c=7&r=0&o=5&pid=1.7" alt="" />
+                                <span>photo_2024_2.png</span>
+                            </div>
+                            <img src="./download.png" alt="" className="icon"/>
+                        </div>
+                        <div className="photoItem">
+                                <div className="photoDetail">
+                                <img src="https://th.bing.com/th/id/OIP.CPCNF0WiJPREiMck_babDAHaEJ?w=292&h=180&c=7&r=0&o=5&pid=1.7" alt="" />
+                                <span>photo_2024_2.png</span>
+                            </div>
+                            <img src="./download.png" alt="" className="icon"/>
+                        </div>
+                        <div className="photoItem">
+                                <div className="photoDetail">
+                                <img src="https://th.bing.com/th/id/OIP.CPCNF0WiJPREiMck_babDAHaEJ?w=292&h=180&c=7&r=0&o=5&pid=1.7" alt="" />
+                                <span>photo_2024_2.png</span>
+                            </div>
+                            <img src="./download.png" alt="" className="icon"/>
+                        </div>
+                    </div>
+                </div>
+                <div className="option">
+                    <div className="title">
+                        <span>Shared Files</span>
+                        <img src="./arrowUp.png" alt="" />
+                    </div>
+                </div>
+                <button>Block User</button>
+                <button className="logout">Logout</button>
+            </div>
+        </div>
     )
 }
 
